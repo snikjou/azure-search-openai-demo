f7beb0353609a5b133b297dc9ffcdfd24b36cfd3
Said Nikjou <44277963+snikjou@users.noreply.github.com>
2025-01-23 15:23:29 +0000

    logo

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -370,7 +370,7 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <img src="CALottLogo.svg" alt="Lottery logo" style={{ width: "120px", height: "120px" }} />
+                            <SparkleFilled fontSize={"120px"} primaryFill={"rgb(115, 225, 161)"} aria-hidden="true" aria-label="Chat logo" />
                             <h1 className={styles.chatEmptyStateTitle}>{t("chatEmptyStateTitle")}</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>{t("chatEmptyStateSubtitle")}</h2>
                             {showLanguagePicker && <LanguagePicker onLanguageChange={newLang => i18n.changeLanguage(newLang)} />}
